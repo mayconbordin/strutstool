50b81568d44c74193ef8c934bdbd1de6888fcb89
mayconbordin <mayconbordin@gmail.com>
2011-05-02 03:24:53 +0000

    Cleaning old useless stuff.

--- standardProject/src/java/com/application/model/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.application.model.repository;
-
-import com.application.model.entity.Usuario;
-import com.framework.util.repository.GenericRepository;
-
-public interface UsuarioRepository extends GenericRepository<Usuario, Integer>  {
-    
-}
